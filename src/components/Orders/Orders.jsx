fe5efa367a54a7cc033284cad1d5cf9f54c1d9fd
AviaryS <93833608+AviaryS@users.noreply.github.com>
2023-02-03 21:07:24 +0300

    Add files via upload

---
+++ src/components/Orders/Orders.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./style.css";
+
+const Orders = ({ orders }) => {
+    return (
+        <div className="orders">
+            <div className="orders__inner">
+                <Link to="/">На главную</Link>
+                {
+                    orders.map((item) => {
+                    return (
+                        <div className="orders__item">
+                            <h2>{item.title}</h2>
+                            <p>{item.price} руб.</p>
+                        </div>
+                    );
+                    })
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Orders;
---
+++ src/components/Products/ProductsPage.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+import './style.css'
+
+const ProductsPage = ({ userAuth, products, setCart, cart }) => {
+    const addToCart = (item) => {
+        if (cart.find((x) => x.product.id === item.id) !== undefined) {
+            const cartItem = cart.find((x) => x.product.id === item.id);
+            let newCartItem = {
+                ...cartItem,
+                price: +cartItem.price + +item.price,
+                quantity: +cartItem.quantity + 1,
+            };
+            setCart(
+                cart.map((x) =>
+                    x.product.id === cartItem.product.id ? newCartItem : x
+                )
+            );
+        } else {
+            const cartItem = { product: item, quantity: 1, price: item.price };
+            setCart([...cart, cartItem]);
+        }
+    };
+
+    return (
+        <div className="home">
+            <div className="home__inner">
+                {products.map((item) => {
+                    return (
+                        <div className="product" key={item.id}>
+                            <h2>{item.title}</h2>
+                            <p>{item.description}</p>
+                            <p>{item.price} руб.</p>
+                            {userAuth && (
+                                <button onClick={() => addToCart(item)}>Add to cart</button>
+                            )}
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default ProductsPage;
